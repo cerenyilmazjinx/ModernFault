ef7514570a6ab750b16b1b41dee6514c44dc794e
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 21:51:40 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/modernfault/main.go
@@ -0,0 +1,23 @@
+// cmd/modernfault/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"modernfault/internal/modernfault"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := modernfault.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
